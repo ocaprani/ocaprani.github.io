e1a5df790f43169e72c79d5e4502ca1f02e17867
ocaprani <37537933+ocaprani@users.noreply.github.com>
2020-08-05 17:45:03 +0200

    Add files via upload

---
+++ TordenskjoldCookie/scripts/main.js
@@ -0,0 +1,75 @@
+// Tordenskjold som lille
+
+// global variabels
+let image = document.querySelector('img');
+let heightLarge = 300;
+let heightSmall = 100;
+let text = document.querySelector('p');
+let textSmall = 'Klik på billedet hvis du vil se Tordenskjold som lille';
+let textLarge = 'Klik på billedet hvis du vil se Tordenskjold som stor ';
+
+image.onclick = function() {
+
+    // local variabel
+    let height = image.getAttribute('height');
+    
+    if( height == heightLarge) {
+	image.setAttribute ('height', heightSmall);
+	text.textContent = textLarge;
+    } else {
+	image.setAttribute ('height', heightLarge);
+	text.textContent = textSmall; 
+    }
+}
+
+function setCookie(cname,cvalue,exdays) {
+    
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+function getCookie(cname) {
+    
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+    
+  var size = getCookie("imageSize");
+    
+  if (size == "") {
+	
+    // Set large image, textSmall and imageSize cookie	
+    image.setAttribute ('height', heightLarge);
+    text.textContent = textSmall;
+    setCookie("imageSize", "Large", 30);
+  
+  } else {
+
+    // Set state according to the value of the cookie
+    image.setAttribute('height', size);
+      
+    if( size == "Small") {
+	text.textContent = textLarge;
+    } else {
+	text.textContent = textSmall; 
+    }
+  }
+}
+
+checkCookie();
